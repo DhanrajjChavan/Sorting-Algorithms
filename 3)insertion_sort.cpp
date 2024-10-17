6eda39eec55aa3dfdab86e707f53bb6dddd77137
DhanrajjChavan <dhanarajchavan777@gmail.com>
2024-10-17 11:14:11 +0530

    Update 3)insertion_sort.cpp

--- 3)insertion_sort.cpp
+++ 3)insertion_sort.cpp
@@ -6,29 +6,27 @@ int main()
     int arr[5] = {45, 23, 86, 12, 9};
     int n = 5;
 
-    // Insertion Sort
     for (int i = 1; i < n; i++) 
     {
-        int current = arr[i]; // The current element to be inserted
+        int current = arr[i]; 
         int j = i - 1;
 
-        // Shift elements of arr[0..i-1] that are greater than current
-        // to one position ahead of their current position
+       
         while (j >= 0 && arr[j] > current) 
         {
             arr[j + 1] = arr[j];
             j--;
         }
-        arr[j + 1] = current; // Insert the current element at its correct position
+        arr[j + 1] = current; 
     }
 
-    // Output the sorted array
+   
     cout << "The sorted array is: ";
     for (int i = 0; i < n; i++) 
     {
         cout << arr[i] << " ";
     }
-    cout << endl; // Newline for better output formatting
+    cout << endl; 
 
     return 0;
 }
