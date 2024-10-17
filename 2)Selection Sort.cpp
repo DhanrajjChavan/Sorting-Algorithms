1e23abdcb869097551295d203947735ac34055e0
DhanrajjChavan <dhanarajchavan777@gmail.com>
2024-10-17 11:13:36 +0530

    Update 2)Selection Sort.cpp

--- 2)Selection Sort.cpp
+++ 2)Selection Sort.cpp
@@ -7,27 +7,25 @@ int main() {
     int arr[5] = {23, 12, 56, 144, 78};
     int n = 5;
 
-    // Selection Sort
     for (int i = 0; i < n - 1; i++) {
-        int min_index = i; // Assume the minimum is the first element of the unsorted array
+        int min_index = i; unsorted array
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_index]) {
-                min_index = j; // Update the index of the minimum element
+                min_index = j; 
             }
         }
-        // Swap the found minimum element with the first element
+
         if (i != min_index) {
             swap(arr[i], arr[min_index]);
         }
     }
 
-    // Output the sorted array
     cout << "The sorted array is: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    cout << endl; // Newline for better output formatting
-
+    cout << endl; 
+    
     return 0;
 }
 
